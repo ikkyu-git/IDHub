457c53c9eddc136b05ea30a4c4aaf691ed9cd714
Natchanon Phongsuea <227080838+ikkyu-git@users.noreply.github.com>
2026-01-30 21:18:16 +0700

    Fix: avoid url() in config/saml.php (use APP_URL) to prevent Request null errors

--- config/saml.php
+++ config/saml.php
@@ -1,7 +1,9 @@
 <?php
 return [
-    'entity_id' => env('SAML_ENTITY_ID', url('/')),
-    'assertion_consumer_service' => env('SAML_ACS_URL', url('/saml/acs')),
+    // Avoid calling url() here because config files are loaded in console
+    // contexts where the request may be null. Use APP_URL as fallback.
+    'entity_id' => env('SAML_ENTITY_ID', rtrim(env('APP_URL', ''), '/')),
+    'assertion_consumer_service' => env('SAML_ACS_URL', rtrim(env('APP_URL', ''), '/') . '/saml/acs'),
     'certificate_path' => storage_path('saml/public.crt'),
     'private_key_path' => storage_path('saml/private.key'),
     'signature_algorithm' => env('SAML_SIGNATURE_ALG', 'http://www.w3.org/2001/04/xmldsig-more#rsa-sha256'),
